e677b86bce23ede187a35de1e2c8a3ba829e82a5
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-09-05 21:32:26 +0900

    No.15: HTMLのID:BudgetのinputとentryDropdownの値を取得

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+/**
+ * HTMLからFormの内容を取得
+ */
 const calorieCounter = document.getElementById("calorie-counter");
+
+/** HTMLからID:Budgetのinputに入力された値を取得 */
+const budgetNumberInput = document.getElementById("budget");
+
+const entryDropdown = document.getElementById("entry-dropdown");
+
+// todo　この検証をしたいのにできなかった
+// console.log({ calorieCounter });
+// console.log(budgetNumberInput.value);
