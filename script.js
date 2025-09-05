752a4a3f9db5b76968bf40001512d8cb1d5a90f0
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-09-05 21:40:12 +0900

    No.16: ボタンとOutputをHTMLから取得

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const budgetNumberInput = document.getElementById("budget");
 
 const entryDropdown = document.getElementById("entry-dropdown");
 
+const addEntryButton = document.getElementById("add-entry");
+
+const clearButton = document.getElementById("clear");
+
+const output = document.getElementById("output");
+
 // todo　この検証をしたいのにできなかった
 // console.log({ calorieCounter });
 // console.log(budgetNumberInput.value);
