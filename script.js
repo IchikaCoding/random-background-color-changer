d9dbf99c65d78fe86378b7381e34c3e07570d815
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-09-05 22:09:56 +0900

    No.18: 文字列を数値に直す処理を追加

--- script.js
+++ script.js
@@ -2,20 +2,19 @@
  * HTMLからFormの内容を取得
  */
 const calorieCounter = document.getElementById("calorie-counter");
-
 /** HTMLからID:Budgetのinputに入力された値を取得 */
 const budgetNumberInput = document.getElementById("budget");
-
 const entryDropdown = document.getElementById("entry-dropdown");
-
 const addEntryButton = document.getElementById("add-entry");
-
 const clearButton = document.getElementById("clear");
-
 const output = document.getElementById("output");
-
 let isError = false;
-
 // todo　この検証をしたいのにできなかった
 // console.log({ calorieCounter });
 // console.log(budgetNumberInput.value);
+
+/**
+ * 文字列を数値に直す処理
+ * @param {string} str
+ */
+function cleanInputString(str) {}
