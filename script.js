9b6bde044170388d606b496d233913a9ae0db455
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-09-19 22:57:44 +0900

    No.80: カロリー過剰と赤字の判定をする処理を追加

--- script.js
+++ script.js
@@ -110,6 +110,7 @@ function calculateCalories(e) {
     breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories =
     budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
 }
 
 /**
